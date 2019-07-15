ac2eeef0786df903b7011e96b48197b4d01fda36
Rudradev Pal <rudradev.pal@gmail.com>
2019-07-15 16:56:56 +0530

    deploy script added

---
+++ deploy-cluster.sh
@@ -0,0 +1,9 @@
+MASTERS=3
+NAMESPACE="redis-cluster"
+NODES=$(($MASTERS * 2))
+
+kubectl create namespace $NAMESPACE
+kubectl apply -f redis-cluster.yml -n $NAMESPACE
+kubectl get pods -n $NAMESPACE
+kubectl scale statefulset redis-cluster --replicas=$NODES -n $NAMESPACE
+kubectl exec -it redis-cluster-0 -n $NAMESPACE -- redis-cli --cluster create --cluster-replicas 1  $(kubectl get pods -l app=redis-cluster-o jsonpath='{range.items[*]} {.status.podIP}:6379' -n $NAMESPACE)
