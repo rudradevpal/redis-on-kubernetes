80848c80b8660641099bf1347ea9c9e094686399
Rudradev Pal <rudradev.pal@gmail.com>
2019-07-16 13:32:21 +0530

    deploy-cluster.sh modified

--- deploy-cluster.sh
+++ deploy-cluster.sh
@@ -4,12 +4,12 @@ MASTERS=3
 NAMESPACE="redis-cluster"
 
 NODES=$(($MASTERS*2))
-echo "Creating Namespace 'redis-cluster'"
+echo "> Creating Namespace 'redis-cluster'"
 kubectl create namespace $NAMESPACE
-echo "Creating ConfigMap, Service & StatefulSet"
+echo "> Creating ConfigMap, Service & StatefulSet"
 kubectl apply -f redis-cluster.yml -n $NAMESPACE
 
-echo "Checking service state"
+echo "> Checking service state"
 while true
 do
   rows=$(kubectl get pods -n $NAMESPACE|awk '{if(NR>1)print}'|grep 'redis-cluster'|awk '{ print $2}'|grep '1/1'|wc -l)
@@ -21,10 +21,10 @@ do
   fi
 done
 
-echo "Scaling redis-cluster to "$NODES
+echo "> Scaling redis-cluster to "$NODES
 kubectl scale statefulset redis-cluster --replicas=$NODES -n $NAMESPACE
 
-echo "Checking readiness of all replicas"
+echo "> Checking readiness of all replicas"
 while true
 do
   rows=$(kubectl get pods -n $NAMESPACE| awk '{if(NR>1)print}'|grep 'redis-cluster'|awk '{ print $2}'|grep '1/1'|wc -l)
@@ -36,5 +36,5 @@ do
   fi
 done
 
-echo "Creating the cluster"
+echo "> Creating the cluster"
 kubectl exec -it redis-cluster-0 -n $NAMESPACE -- redis-cli --cluster create --cluster-replicas 1  $(kubectl get pods -l app=redis-cluster -o jsonpath='{range.items[*]} {.status.podIP}:6379' -n $NAMESPACE) <<< "yes"
