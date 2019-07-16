f137adbc94fa1d413fc1f64ea1fdf279cf1ec3aa
Rudradev Pal <rudradev.pal@gmail.com>
2019-07-16 12:57:53 +0530

    deploy-cluster.sh modified

--- deploy-cluster.sh
+++ deploy-cluster.sh
@@ -1,8 +1,12 @@
+# No of master node. The same no of Slave will also be created.
 MASTERS=3
+# Namespace to launch the cluster. The namespace should not exist in Kubernetes
 NAMESPACE="redis-cluster"
 
 NODES=$(($MASTERS*2))
+echo "Creating Namespace 'redis-cluster'"
 kubectl create namespace $NAMESPACE
+echo "Creating ConfigMap, Service & StatefulSet"
 kubectl apply -f redis-cluster.yml -n $NAMESPACE
 
 while true
@@ -16,9 +20,10 @@ do
   fi
 done
 
-# kubectl get pods -n $NAMESPACE
+echo "Scaling redis-cluster to "$NODES
 kubectl scale statefulset redis-cluster --replicas=$NODES -n $NAMESPACE
 
+echo "Checking readiness of all replicas"
 while true
 do
   rows=$(kubectl get pods -n $NAMESPACE| awk '{if(NR>1)print}'|grep 'redis-cluster'|awk '{ print $2}'|grep '1/1'|wc -l)
@@ -30,4 +35,5 @@ do
   fi
 done
 
+echo "Creating the cluster"
 kubectl exec -it redis-cluster-0 -n $NAMESPACE -- redis-cli --cluster create --cluster-replicas 1  $(kubectl get pods -l app=redis-cluster -o jsonpath='{range.items[*]} {.status.podIP}:6379' -n $NAMESPACE) <<< "yes"
